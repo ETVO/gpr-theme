df3dd4b632c41a887b63886c8f8e4e9969734426
Estevão P. Rolim <estevao.prolim@gmail.com>
2024-04-15 10:57:00 +0100

    set parceiros bg white

--- inc/shortcodes/blocks/parceiros.php
+++ inc/shortcodes/blocks/parceiros.php
@@ -9,7 +9,7 @@ function parceiros($attrs)
 
     ob_start(); // Start HTML buffering
 ?>
-    <section class="shoco-dna py-5">
+    <section class="shoco-dna py-5 bg-white">
         <div class="container col-12 col-md-10 col-xl-8 mx-auto py-3">
             <div class="title text-center mb-4">
                 <h2 class="fs-1"><?php echo $parceiros_title; ?></h2>
